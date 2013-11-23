efdfb6586626af5c23d1ac6555326274b1ebd626
Steven Allen <steven@stebalien.com>
2013-11-23 13:37:01 -0500

    Add copyright.

--- overkill/extra/writers.py
+++ overkill/extra/writers.py
@@ -1,3 +1,20 @@
+##
+#    This file is part of Overkill-writers.
+#
+#    Overkill-writers is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Overkill-writers is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Overkill-writers.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 from ..base import Runnable
 import subprocess
 
